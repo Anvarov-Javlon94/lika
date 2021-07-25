529c4c26bbfc0a629412574e50a5adb249e6c5d6
Javlon Anvarov <anvarov.javlon94@gmail.com>
2021-07-25 16:49:20 +0500

    Second

--- src/main/java/sqb/uz/marry_me/service/ClientService.java
+++ src/main/java/sqb/uz/marry_me/service/ClientService.java
@@ -3,6 +3,7 @@ package sqb.uz.marry_me.service;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import sqb.uz.marry_me.entity.ClientEntity;
 import sqb.uz.marry_me.repository.ClientRepository;
@@ -11,12 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ClientService {
 
     ClientRepository clientRepository;
 
     public void addClientToDateBase(HttpServletRequest request) {
+        log.info("Success");
         ClientEntity clientEntity = new ClientEntity();
         clientEntity.setIp_address(request.getRemoteAddr());
         clientEntity.setLocal_name(request.getLocalName());
